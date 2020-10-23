d55ca20f838c86ebd2238d4b96e4af83a132a1e6
Joey Brown <gjoey.brown@gmail.com>
2020-10-23 13:44:22 -0700

    created html routes and tested

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    })
+
+    // app.get("/reserve", function (req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    // });
+
+    // // If no matching route is found default to home
+    // app.get("*", function (req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/home.html"));
+    // });
+};
